75e506875ebe0f5128909a5e44f156580f2af60b
adisornS <sorn130839@gmail.com>
2019-03-04 14:51:34 +0700

    Register Success

---
+++ app/src/main/java/masterung/th/in/androidthai/bsruservice/MyConstant.java
@@ -0,0 +1,10 @@
+package masterung.th.in.androidthai.bsruservice;
+
+public class MyConstant {
+
+    private String urlAddUser = "http://androidthai.in.th/bsru/addUserMaster.php";
+
+    public String getUrlAddUser() {
+        return urlAddUser;
+    }
+}
